080af1a48350bcc59fcec65c4d46b459fb811e0c
Biyuan Ding <bding@amazon.com>
2023-01-17 23:32:50 +0000

    feat(telescope): Add more mappings

--- init.lua
+++ init.lua
@@ -200,8 +200,9 @@ end)
 
 try_require('telescope.builtin', function(builtin)
   vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-  vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+  vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
   vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+  vim.keymap.set('n', '<leader>:', builtin.commands, {})
 end)
 
 -- Set up auto completion.
