9a2ccf13f0b8da2eeb2384adf38ee39839b4ad1e
Ian Ding <ianding1@icloud.com>
2024-09-19 17:49:52 -0700

    feat: Move configuration to packer config

--- init.lua
+++ init.lua
@@ -80,6 +80,11 @@ end
 -- Make sure packer.nvim has been installed.
 local packer_bootstrap = ensure_packer()
 
+local keymap_opts = {
+  noremap = true,
+  silent = true,
+}
+
 -- Plugin list.
 require("packer").startup(function(use)
   -- The package manager itself.
@@ -88,21 +93,86 @@ require("packer").startup(function(use)
   use("nvim-lua/plenary.nvim")
 
   -- Color scheme.
-  use("rebelot/kanagawa.nvim")
+  use({
+    "rebelot/kanagawa.nvim",
+    config = function()
+      vim.cmd("colorscheme kanagawa-dragon")
+    end,
+  })
 
   -- Undotree UI. Visualize the undo history as a tree.
-  use("mbbill/undotree")
+  use({
+    "mbbill/undotree",
+    config = function()
+      vim.keymap.set("n", "<space>u", "<Cmd>UndotreeToggle<CR>", keymap_opts)
+    end,
+  })
 
   -- File manager.
-  use("stevearc/oil.nvim")
+  use({
+    "stevearc/oil.nvim",
+    config = function()
+      require("oil").setup()
+      vim.keymap.set("n", "-", "<Cmd>Oil<CR>")
+    end,
+  })
 
   -- Enhance the writing experience.
-  use("preservim/vim-pencil")
-  use("folke/zen-mode.nvim")
+  use({
+    "preservim/vim-pencil",
+    config = function()
+      vim.keymap.set("n", "<space>pp", "<Cmd>PencilToggle<CR>")
+      vim.keymap.set("n", "<space>ph", "<Cmd>PencilHard<CR>")
+      vim.keymap.set("n", "<space>ps", "<Cmd>PencilSoft<CR>")
+      vim.keymap.set("n", "<space>po", "<Cmd>PencilOff<CR>")
+    end,
+  })
+
+  use({
+    "folke/zen-mode.nvim",
+    config = function()
+      local zen_mode = require("zen-mode")
+      vim.keymap.set("n", "<space>j", function()
+        zen_mode.toggle({
+          window = { width = 80 },
+        })
+      end)
+      vim.keymap.set("n", "<space>k", zen_mode.toggle)
+    end,
+  })
 
   -- Powerful fuzzy finder.
-  use({ "junegunn/fzf", run = "./install --bin" })
-  use("ibhagwan/fzf-lua")
+  use({
+    "ibhagwan/fzf-lua",
+    requires = { { "junegunn/fzf", run = "./install --bin" } },
+    config = function()
+      local fzf = require("fzf-lua")
+      fzf.setup({
+        winopts = {
+          preview = {
+            layout = "vertical",
+          },
+        },
+        files = { cwd_prompt = false },
+      })
+
+      -- File/buffer/glob fuzzy search.
+      -- Uses home row keys s/d/f.
+      vim.keymap.set("n", "<space>f", fzf.files)
+      vim.keymap.set("n", "<space>d", fzf.buffers)
+      vim.keymap.set("n", "<space>s", fzf.live_grep_glob)
+      vim.keymap.set("n", "<space>re", fzf.resume)
+
+      -- Bind LSP actions to FZF.
+      vim.keymap.set("n", "gD", fzf.lsp_typedefs)
+      vim.keymap.set("n", "gd", fzf.lsp_definitions)
+      vim.keymap.set("n", "gi", fzf.lsp_implementations)
+      vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help)
+      vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+      vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename)
+      vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action)
+    end,
+  })
 
   -- VSCode-like icons for Git, file types, and etc.
   -- Patched fonts are required for these icons to be rendered correctly.
@@ -110,7 +180,16 @@ require("packer").startup(function(use)
   use("nvim-tree/nvim-web-devicons")
 
   -- Auto close parentheses, brackets and tags.
-  use("windwp/nvim-autopairs")
+  use({
+    "windwp/nvim-autopairs",
+    config = function()
+      local autopairs = require("nvim-autopairs")
+      autopairs.setup({})
+
+      -- Disable closing single quotes on ocaml files.
+      autopairs.get_rule("'")[1].not_filetypes = { "ocaml" }
+    end,
+  })
 
   -- A very powerful Git plugin for Vim.
   -- See http://vimcasts.org/categories/git/ for a series of awesome tutorials on fugitive.
@@ -125,14 +204,114 @@ require("packer").startup(function(use)
   -- coc.nvim, which was created before Neovim LSP was added, closed in the gap for Vim LSP support, and has maintained an active community till today.
   --
   -- I don't use snippets personally but Neovim LSP requires a snippet engine.
-  use("williamboman/mason.nvim")
-  use("williamboman/mason-lspconfig.nvim")
-  use("neovim/nvim-lspconfig")
   use("hrsh7th/cmp-nvim-lsp")
-  use("hrsh7th/nvim-cmp")
+  use({
+    "hrsh7th/nvim-cmp",
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      local lspkind = require("lspkind")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-d>"] = cmp.mapping.scroll_docs(4),
+          ["<C-c>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          -- Bind <Tab> to selecting the next candidate.
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          -- Bind <Super-Tab> to selecting the previous candidate.
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = "symbol",
+            maxwidth = 50,
+            ellipsis_char = "...",
+          }),
+        },
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+        }),
+      })
+    end,
+  })
   use("saadparwaiz1/cmp_luasnip")
   use("onsails/lspkind.nvim")
   use("L3MON4D3/LuaSnip")
+
+  use("williamboman/mason.nvim")
+  use("williamboman/mason-lspconfig.nvim")
+  use({
+    "neovim/nvim-lspconfig",
+    config = function()
+      local mason = require("mason")
+      local mason_lspconfig = require("mason-lspconfig")
+      local lspconfig = require("lspconfig")
+      mason.setup()
+      mason_lspconfig.setup({
+        ensure_installed = { "lua_ls" },
+        automatic_installation = false,
+      })
+
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      capabilities.textDocument.foldingRange = {
+        dynamicRegistration = false,
+        lineFoldingOnly = true,
+      }
+
+      -- Automatically set up LSP servers installed via mason.
+      mason_lspconfig.setup_handlers({
+        -- Default set up handler.
+        function(server_name)
+          lspconfig[server_name].setup({
+            capabilities = capabilities,
+          })
+        end,
+        lua_ls = function()
+          lspconfig.lua_ls.setup({
+            capabilities = capabilities,
+            settings = {
+              Lua = {
+                runtime = {
+                  version = "LuaJIT",
+                },
+                diagnostics = {
+                  globals = { "vim" },
+                },
+                workspace = {
+                  library = vim.api.nvim_get_runtime_file("", true),
+                },
+                telemetry = {
+                  enable = false,
+                },
+              },
+            },
+          })
+        end,
+      })
+    end,
+  })
 
   -- Syntax highlighting based off of treesitter, a generic parser generator tool that supports a variety
   -- of programming languages.
@@ -143,13 +322,66 @@ require("packer").startup(function(use)
       local ts_update = installer.update({ with_sync = true })
       ts_update()
     end,
+    config = function()
+      local treesitter = require("nvim-treesitter.configs")
+      treesitter.setup({
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "markdown", "markdown_inline" },
+        sync_install = false,
+        auto_install = true,
+        ignore_install = {},
+        modules = {},
+        highlight = {
+          enable = true,
+          disable = function(_, buf)
+            -- Disable treesitter when the file size is larger than 1 MB.
+            local max_filesize = 1024 * 1024
+            local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
+          -- Disable Vim's regex-based syntax highlighting when treesitter is enabled.
+          additional_vim_regex_highlighting = false,
+        },
+      })
+    end,
   })
 
   -- Folding.
-  use({ "kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async" })
+  use({
+    "kevinhwang91/nvim-ufo",
+    requires = "kevinhwang91/promise-async",
+    config = function()
+      vim.o.foldlevel = 99
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      local ufo = require("ufo")
+      vim.keymap.set("n", "zR", ufo.openAllFolds)
+      vim.keymap.set("n", "zM", ufo.closeAllFolds)
+      ufo.setup()
+    end,
+  })
 
   -- Formatter.
-  use("stevearc/conform.nvim")
+  use({
+    "stevearc/conform.nvim",
+    config = function()
+      require("conform").setup({
+        format_on_save = {
+          timeout_ms = 500,
+          lsp_fallback = true,
+        },
+        formatters_by_ft = {
+          lua = { "stylua" },
+          python = { "black" },
+          typescript = { "prettierd", "prettier", stop_after_first = true },
+        },
+      })
+
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
+  })
 
   -- Run :PackerSync if it is the first time.
   -- You still need to run :PackerSync from time to time to keep all the plugins
@@ -164,207 +396,3 @@ if packer_bootstrap then
   print("Please restart Neovim to load the plugins just installed.")
   return
 end
-
--- Set color scheme to kanagawa (the dark variant).
-vim.cmd("colorscheme kanagawa-dragon")
-
--- Set up undotree.
-vim.keymap.set("n", "<space>u", ":UndotreeToggle<CR>", { noremap = true })
-
--- Set up autopairs.
-local autopairs = require("nvim-autopairs")
-autopairs.setup({})
-
--- Disable closing single quotes on ocaml files.
-autopairs.get_rule("'")[1].not_filetypes = { "ocaml" }
-
--- Set up pencil.
-vim.keymap.set("n", "<space>pp", "<Cmd>PencilToggle<CR>")
-vim.keymap.set("n", "<space>ph", "<Cmd>PencilHard<CR>")
-vim.keymap.set("n", "<space>ps", "<Cmd>PencilSoft<CR>")
-vim.keymap.set("n", "<space>po", "<Cmd>PencilOff<CR>")
-
--- Set up Zen mode.
--- Uses home row keys j/k.
-local zen_mode = require("zen-mode")
-vim.keymap.set("n", "<space>j", function()
-  zen_mode.toggle({
-    window = { width = 80 },
-  })
-end)
-vim.keymap.set("n", "<space>k", zen_mode.toggle)
-
--- Set up oil.
-local oil = require("oil")
-oil.setup()
-vim.keymap.set("n", "-", "<Cmd>Oil<CR>")
-
--- Set up FZF.
-local fzf = require("fzf-lua")
-fzf.setup({
-  winopts = {
-    preview = {
-      layout = "vertical",
-    },
-  },
-  files = { cwd_prompt = false },
-})
-
--- File/buffer/glob fuzzy search.
--- Uses home row keys s/d/f.
-vim.keymap.set("n", "<space>f", fzf.files)
-vim.keymap.set("n", "<space>d", fzf.buffers)
-vim.keymap.set("n", "<space>s", fzf.live_grep_glob)
-vim.keymap.set("n", "<space>re", fzf.resume)
-
--- Bind LSP actions to FZF.
-vim.keymap.set("n", "gD", fzf.lsp_typedefs)
-vim.keymap.set("n", "gd", fzf.lsp_definitions)
-vim.keymap.set("n", "gi", fzf.lsp_implementations)
-vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help)
-vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
-vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename)
-vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action)
-
--- Set up auto completion.
-local cmp = require("cmp")
-local luasnip = require("luasnip")
-local lspkind = require("lspkind")
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-d>"] = cmp.mapping.scroll_docs(4),
-    ["<C-c>"] = cmp.mapping.abort(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    -- Bind <Tab> to selecting the next candidate.
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    -- Bind <Super-Tab> to selecting the previous candidate.
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  }),
-  formatting = {
-    format = lspkind.cmp_format({
-      mode = "symbol",
-      maxwidth = 50,
-      ellipsis_char = "...",
-    }),
-  },
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-  }),
-})
-
--- Set up treesitter.
-local treesitter = require("nvim-treesitter.configs")
-treesitter.setup({
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "markdown", "markdown_inline" },
-  sync_install = false,
-  auto_install = true,
-  ignore_install = {},
-  modules = {},
-  highlight = {
-    enable = true,
-    disable = function(_, buf)
-      -- Disable treesitter when the file size is larger than 1 MB.
-      local max_filesize = 1024 * 1024
-      local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
-      if ok and stats and stats.size > max_filesize then
-        return true
-      end
-    end,
-    -- Disable Vim's regex-based syntax highlighting when treesitter is enabled.
-    additional_vim_regex_highlighting = false,
-  },
-})
-
--- Configure folding.
-vim.o.foldlevel = 99
-vim.o.foldlevelstart = 99
-vim.o.foldenable = true
-
-local ufo = require("ufo")
-vim.keymap.set("n", "zR", ufo.openAllFolds)
-vim.keymap.set("n", "zM", ufo.closeAllFolds)
-ufo.setup()
-
--- Set up LSP.
-local mason = require("mason")
-local mason_lspconfig = require("mason-lspconfig")
-local lspconfig = require("lspconfig")
-mason.setup()
-mason_lspconfig.setup({
-  ensure_installed = { "lua_ls" },
-  automatic_installation = false,
-})
-
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-capabilities.textDocument.foldingRange = {
-  dynamicRegistration = false,
-  lineFoldingOnly = true,
-}
-
--- Automatically set up LSP servers installed via mason.
-mason_lspconfig.setup_handlers({
-  -- Default set up handler.
-  function(server_name)
-    lspconfig[server_name].setup({
-      capabilities = capabilities,
-    })
-  end,
-  lua_ls = function()
-    lspconfig.lua_ls.setup({
-      capabilities = capabilities,
-      settings = {
-        Lua = {
-          runtime = {
-            version = "LuaJIT",
-          },
-          diagnostics = {
-            globals = { "vim" },
-          },
-          workspace = {
-            library = vim.api.nvim_get_runtime_file("", true),
-          },
-          telemetry = {
-            enable = false,
-          },
-        },
-      },
-    })
-  end,
-})
-
--- Configure Formatter
-require("conform").setup({
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
-  formatters_by_ft = {
-    lua = { "stylua" },
-    python = { "black" },
-    typescript = { "prettierd", "prettier", stop_after_first = true },
-  },
-})
-
-vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
